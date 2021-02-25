681b8cbbc0a12792acdc0b27dedaffc55f990d78
GokulJaeger <gokuljaeger@gmail.com>
2021-02-26 00:54:55 +0530

    first commit

---
+++ first_script.js
@@ -0,0 +1,3 @@
+var nam = "Gokul";
+var age = 23;
+console.log("My name is " + nam + " and I am " + age + " old. Please to meet you.");
---
+++ first_script.ts
@@ -0,0 +1,3 @@
+let nam:string = "Gokul";
+let age:number = 23;
+console.log("My name is "+nam+ " and I am "+age+" old. Please to meet you.");
