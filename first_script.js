ef6c0db26e3c0c01e8a59d89b62edca76a5e66f3
GokulJaeger <gokuljaeger@gmail.com>
2021-02-26 01:03:20 +0530

    Updated my first_script to my TypeScript repository

--- first_script.js
+++ first_script.js
@@ -1,3 +1,6 @@
 var nam = "Gokul";
 var age = 23;
-console.log("My name is " + nam + " and I am " + age + " old. Please to meet you.");
+var company = "SRM Technologies";
+var group = "RightStroke";
+console.log("My name is " + nam + " and I'm " + age + " old. Pleased to meet you.");
+console.log("This is my First Script in Type Script during my first day of Angular introduction from " + group + " organised by " + company);
--- first_script.ts
+++ first_script.ts
@@ -1,3 +1,6 @@
 let nam:string = "Gokul";
 let age:number = 23;
-console.log("My name is "+nam+ " and I am "+age+" old. Please to meet you.");
+let company:string ="SRM Technologies";
+let group:string = "RightStroke";
+console.log(`My name is ${nam} and I'm ${age} old. Pleased to meet you.`);
+console.log(`This is my First Script in Type Script during my first day of Angular introduction from ${group} organised by ${company}`);
